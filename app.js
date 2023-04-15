7ac2f7a4c61f85633a6903a97e46725d5c4a3a91
manikchand-tech <101378045+manikchand-tech@users.noreply.github.com>
2023-04-15 11:09:15 +0530

    added eventlistner

--- app.js
+++ app.js
@@ -3,5 +3,9 @@ var btn = document.querySelector('#btn-translate');
 var intxt = document.querySelector('#inputtxt');
 var outtext = document.querySelector('#outputtxt');
 var username;
-username = prompt("enter " + "username :");
-alert("welcome " + username + " ! ");
+// username = prompt("enter " + "username :");
+// alert("welcome " + username + " ! ");
+
+btn.addEventListener("click", function myListner() {
+    console.log("clicked");
+})
