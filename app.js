1bc63af4a2b371d68630ee1d3a97d5ae6228aa02
manikchand-tech <101378045+manikchand-tech@users.noreply.github.com>
2023-04-15 10:27:32 +0530

    prompt,alert

---
+++ app.js
@@ -0,0 +1,7 @@
+
+var btn = document.querySelector('#btn-translate');
+var intxt = document.querySelector('#inputtxt');
+var outtext = document.querySelector('#outputtxt');
+var username;
+username = prompt("enter " + "username :");
+alert("welcome " + username + " ! ");
